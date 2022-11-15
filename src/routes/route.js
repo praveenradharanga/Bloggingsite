9bba342658a93f2972b63543f846fe81bc34653c
Harsh-Kumar-Sharma <gaurharsh1737@gmail.com>
2022-11-15 21:22:36 +0530

    create new api

--- src/routes/route.js
+++ src/routes/route.js
@@ -3,11 +3,16 @@ const router=express.Router()
 //middlewares
 const blogValidations=require('../Middlewares/blogValidations')
 const authorValidations=require('../Middlewares/authorValidations')
+const auth= require("../Middlewares/auth")
 //Controllers
 const createBlog=require('../controllers/blogController')
 const createAuthor=require('../controllers/authorController')
 
-router.post("/blogs/create-blog",blogValidations.blogValidator,createBlog.createBlog)
+router.post("/blogs/create-blog",auth.mid1,blogValidations.blogValidator,createBlog.createBlog)
 router.post("/blogs/create-author",authorValidations.autorValidator,createAuthor.createAuthor)
+ router.get("/getblog", createBlog.getblog)
+ router.post("/login",createAuthor.loginAuthor)
+ router.put("/blogs/update-blog/:blogId",auth.mid1,auth.mid2,createBlog.updateBlog)
+
 
 module.exports=router
